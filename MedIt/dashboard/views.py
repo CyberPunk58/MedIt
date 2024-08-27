c753330be2f67c8c292e1e5a0ff533bb91bbdd39
CyberPunk58 <pavelrip@mail.ru>
2024-08-27 12:03:58 +0300

    sql light added

--- MedIt/dashboard/views.py
+++ MedIt/dashboard/views.py
@@ -10,20 +10,29 @@ def dashboard_view(request):
 
 @permission_required('dashboard.add_revenue', raise_exception=True)
 def dashboard_admin_view(request):
+    overwrite = False  # Инициализируем переменную
+
     if request.method == 'POST':
         form = RevenueForm(request.POST)
         if form.is_valid():
             clinic = form.cleaned_data['clinic']
             date = form.cleaned_data['date']
             payment_type = form.cleaned_data['payment_type']
+            revenue = form.cleaned_data['revenue']
+
             try:
                 existing_revenue = Revenue.objects.get(clinic=clinic, date=date, payment_type=payment_type)
-                messages.warning(request, f'Запись на {date} для {clinic.name} уже существует.')
                 if 'overwrite' in request.POST:
-                    existing_revenue.revenue = form.cleaned_data['revenue']
+                    existing_revenue.revenue = revenue
                     existing_revenue.save()
                     messages.success(request, 'Запись успешно обновлена.')
                     return redirect('dashboard_admin')
+                else:
+                    messages.warning(
+                        request,
+                        f'Запись на {date} для {clinic.name} с типом платежа {payment_type.type} уже существует.'
+                    )
+                    overwrite = True  # Устанавливаем флаг для отображения кнопки "Перезаписать"
             except Revenue.DoesNotExist:
                 form.save()
                 messages.success(request, 'Запись успешно добавлена.')
@@ -31,4 +40,4 @@ def dashboard_admin_view(request):
     else:
         form = RevenueForm()
 
-    return render(request, 'dashboard/dashboard_admin.html', {'form': form})
+    return render(request, 'dashboard/dashboard_admin.html', {'form': form, 'overwrite': overwrite})
